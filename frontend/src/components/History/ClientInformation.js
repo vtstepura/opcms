335a510bd039db90f4eaffdf8ec3a95040f3e314
vtstepura <vtstepura97@gmail.com>
2020-04-15 13:30:10 +0300

    create ClietnInformation

---
+++ frontend/src/components/History/ClientInformation.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ClientInformation = ({ client }) => {
+  console.log(client.client)
+  return (
+    <div style={{ width: '60%', justifyContent: 'space-between', display: 'flex', height: 70, alignItems: 'center' }}>
+      <span style={{ fontWeight: 'bold', fontSize: 18}}>Client:<span style={{ fontWeight: 'normal'}}> {client.name}</span></span>
+      <span style={{ fontWeight: 'bold', fontSize: 18}}>Project:<span style={{ fontWeight: 'normal'}}> {client.project}</span></span>
+      <span style={{ fontWeight: 'bold', fontSize: 18}}>Department:<span style={{ fontWeight: 'normal'}}> {client.department}</span></span>
+    </div>
+  )
+}
+
+
+export default ClientInformation
